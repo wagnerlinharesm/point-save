a04f2bf7f169c580a12c7cfef646a6d3cbe33bb8
lucas vieira <lucas.vss12@gmail.com>
2024-03-19 20:45:58 -0300

    Adicionando logs

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,27 +1,51 @@
+import logging
 import os
-import boto3
-
 from datetime import date
 from datetime import datetime
+
+import boto3
 
 RDS_ARN = os.environ.get('RDS_ARN')
 SECRET_RDS = os.environ.get('SECRET_RDS')
 DATABASE_NAME = os.environ.get('DATABASE_NAME')
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def handler(event, context):
+    logging.info('Iniciando a execução da função lambda.')
+
     id_funcionario = event['id_funcionario']
 
-    # Buscar o Ponto no RDS
     ponto = buscar_ponto(id_funcionario, date.today())
     situacao_pontos = busca_situacao_pontos()
+    periodo_ponto = None
 
     if not ponto:
         situacao_ponto_aberto = next(
             filter(lambda situacao_ponto: situacao_ponto.descricao == 'ABERTO', situacao_pontos)
         )
-        ponto = Ponto(None, id_funcionario, situacao_ponto_aberto.id_situacao_ponto, datetime.now(), None)
-        salvar_ponto(ponto)
+
+        ponto = salvar_ponto(
+            Ponto(
+                None,
+                id_funcionario,
+                situacao_ponto_aberto.id_situacao_ponto,
+                datetime.now(),
+                None
+            )
+        )
+
+        periodo_ponto = salvar_periodo_ponto(
+            PeriodoPonto(
+                None,
+                ponto.id_ponto,
+                datetime.now(),
+                None,
+                0
+            )
+        )
     else:
         periodo_ponto = buscar_periodo_ponto_aberto(ponto.id_ponto)
 
@@ -29,16 +53,25 @@ def handler(event, context):
             periodo_ponto.horario_saida = datetime.now()
             atualizar_periodo_ponto(periodo_ponto)
         else:
-            periodo_ponto = PeriodoPonto(None, ponto.id_ponto, datetime.now(), None, 0)
-            salvar_periodo_ponto(periodo_ponto)
+            periodo_ponto = salvar_periodo_ponto(
+                PeriodoPonto(
+                    None,
+                    ponto.id_ponto,
+                    datetime.now(),
+                    None,
+                    0
+                )
+            )
 
         ponto.horas_trabalhadas += calcular_horas_trabalhadas(periodo_ponto)
         atualizar_ponto(ponto)
 
-    return event
+    return periodo_ponto
 
 
 def buscar_ponto(id_funcionario, data):
+    logging.info('f=buscar_ponto, m=inciando a verificação para ver se existe ponto já criado.')
+
     rds = boto3.client('rds-data')
 
     data_formatada = data.strftime('%Y-%m-%d')
@@ -55,12 +88,18 @@ def buscar_ponto(id_funcionario, data):
     if response['records']:
         ponto_data = response['records'][0]
         ponto = Ponto(ponto_data[0], ponto_data[1], ponto_data[2], ponto_data[3], ponto_data[4])
+
+        logging.info(f'f=buscar_ponto, m=ponto encontrado {ponto}.')
+
         return ponto
 
+    logging.info(f'f=buscar_ponto, m=ponto não encontrado.')
     return None
 
 
 def busca_situacao_pontos():
+    logging.info('f=busca_situacao_pontos, m=inciando a busca das situações.')
+
     rds = boto3.client('rds-data')
 
     sql = f"SELECT * FROM situacao_ponto"
@@ -79,12 +118,16 @@ def busca_situacao_pontos():
             situacao_ponto = SituacaoPonto(situacao_ponto_data[0], situacao_ponto_data[1])
             situacao_pontos.append(situacao_ponto)
 
+        logging.info(f'f=busca_situacao_pontos, m=situações encontradas {situacao_pontos}.')
         return situacao_pontos
 
+    logging.info(f'f=busca_situacao_pontos, m=situações não encontradas.')
     return None
 
 
 def buscar_periodo_ponto_aberto(id_ponto):
+    logging.info('f=buscar_periodo_ponto_aberto, m=inciando a verificação para ver se existe periodo ponto aberto.')
+
     rds = boto3.client('rds-data')
 
     sql = f"SELECT * FROM periodo_ponto WHERE id_ponto = {id_ponto} AND hora_saida is null"
@@ -105,12 +148,17 @@ def buscar_periodo_ponto_aberto(id_ponto):
             periodo_ponto_data[3],
             periodo_ponto_data[4]
         )
+
+        logging.info(f'f=buscar_periodo_ponto_aberto, m=periodo ponto encontrado {periodo_ponto}.')
         return periodo_ponto
 
+    logging.info(f'f=buscar_periodo_ponto_aberto, m=periodo ponto não encontrado.')
     return None
 
 
 def salvar_ponto(ponto):
+    logging.info('f=salvar_ponto, m=iniciando processo para salvar ponto.')
+
     rds = boto3.client('rds-data')
 
     data_formatada = ponto.data.strftime('%Y-%m-%d')
@@ -129,10 +177,13 @@ def salvar_ponto(ponto):
 
     ponto.id_ponto = response['generatedFields'][0]['longValue']
 
+    logging.info(f'f=salvar_ponto, m=ponto salvo com sucesso {ponto}.')
     return ponto
 
 
 def salvar_periodo_ponto(periodo_ponto):
+    logging.info('f=salvar_periodo_ponto, m=iniciando processo para salvar periodo ponto.')
+
     rds = boto3.client('rds-data')
 
     sql = f"""
@@ -149,10 +200,13 @@ def salvar_periodo_ponto(periodo_ponto):
 
     periodo_ponto.id_periodo_ponto = response['generatedFields'][0]['longValue']
 
+    logging.info(f'f=salvar_periodo_ponto, m=periodo ponto salvo com sucesso {periodo_ponto}.')
     return periodo_ponto
 
 
 def atualizar_periodo_ponto(periodo_ponto):
+    logging.info('f=atualizar_periodo_ponto, m=iniciando processo para atualizar periodo ponto.')
+
     rds = boto3.client('rds-data')
 
     horas_periodo = (periodo_ponto.horario_saida - periodo_ponto.horario_entrada).total_seconds() / 3600
@@ -169,6 +223,8 @@ def atualizar_periodo_ponto(periodo_ponto):
         database=DATABASE_NAME,
         sql=sql
     )
+
+    logging.info(f'f=atualizar_periodo_ponto, m=periodo ponto atualizado com sucesso {periodo_ponto}.')
 
 
 def calcular_horas_trabalhadas(id_ponto):
@@ -195,6 +251,8 @@ def calcular_horas_trabalhadas(id_ponto):
 
 
 def atualizar_ponto(ponto):
+    logging.info('f=atualizar_ponto, m=iniciando processo para atualizar ponto.')
+
     rds = boto3.client('rds-data')
 
     sql = f"""
@@ -209,6 +267,8 @@ def atualizar_ponto(ponto):
         database=DATABASE_NAME,
         sql=sql
     )
+
+    logging.info(f'f=atualizar_ponto, m=ponto atualizado com sucesso {ponto}.')
 
 
 class Ponto:

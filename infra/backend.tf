72a1f0a001a00a75859923511cbdf66babd8c0ac
GAPrado <guilhermeprado@outlook.com>
2024-03-20 21:53:56 -0300

    fix tfstate file name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "point-terraform-state"
-    key    = "point-report.tfstate"
+    key    = "point-save.tfstate"
     region = "us-east-2"
     encrypt = true
   }

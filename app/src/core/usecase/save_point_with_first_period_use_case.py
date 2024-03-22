e64dd466a5e2684b11ed01dc536c43180816b641
lucas vieira <lucas.vss12@gmail.com>
2024-03-22 18:14:18 -0300

    Realizando ajustes

--- app/src/core/usecase/save_point_with_first_period_use_case.py
+++ app/src/core/usecase/save_point_with_first_period_use_case.py
@@ -1,3 +1,5 @@
+import logging
+
 from datetime import time
 
 from app.src.adapter.point_adapter import PointAdapter
@@ -6,14 +8,18 @@ from app.src.core.domain.point import Point
 from app.src.core.domain.point_period import PointPeriod
 from app.src.util.singleton_meta import SingletonMeta
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 class SavePointWithFirstPeriodUseCase(metaclass=SingletonMeta):
-
     _point_adapter = PointAdapter()
     _point_period_adapter = PointPeriodAdapter()
 
     def execute(self, employee_id, situations, now) -> None:
         situation = next(filter(lambda element: element.description == 'ABERTO', situations))
+
+        logging.info('situations: %s', situations)
 
         init_time = time(0, 0, 0)
         formatted_date = now.strftime('%Y-%m-%d')
